4efc2e3b9402f54cddd3e954b63feb156aee6d20
ipopotamo <ignaciobarbieri@gmail.com>
2022-09-12 23:37:43 -0300

    hice un update y un delete ayuda me estoy muriendo

---
+++ php/formdelete.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<h1>Registrar Obras</h1>
+		<meta charset="utf-8">
+
+<br><br>
+
+</head>
+<body>
+
+    <form method="post">
+    	<input type="text"   name="nombre"      placeholder="Nombre">
+    	<input type="text"   name="descripcion" placeholder="Descripcion">
+    	<input type="text"   name="sucur"       placeholder="Sucursal">
+    	<input type="text"   name="zona"        placeholder="zona">
+   		<input type="text"   name="piso"        placeholder="piso"> 
+    	<input type="submit" name="boton">
+    </form>
+	
+
+    <?php
+	include("delete.php");
+    ?>
+
+</body>
