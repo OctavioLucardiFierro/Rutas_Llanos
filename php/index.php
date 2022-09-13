7d1b36a8b3ec648c66256ed9ce7471a5debcc0a2
ipopotamo <ignaciobarbieri@gmail.com>
2022-09-12 23:33:44 -0300

    tengo sueño

---
+++ php/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<h1>¿Que va a hacer?</h1>
+		<meta charset="utf-8">
+
+<br><br>
+
+</head>
+<body>
+
+<a href="form.php"><button>INERTAR OBRA</button></a>
+<br>
+<br>
+<a href="form.php"><button>MODIFICAR OBRA</button></a>
+<br>
+<br>
+<a href="form.php"><button>BORRAR OBRA</button></a>
+
+</body>
---
+++ sql/rutas_llanos.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-09-2022 a las 03:13:55
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `rutas_llanos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `obras`
+--
+
+CREATE TABLE `obras` (
+  `id` int(11) NOT NULL,
+  `nombre_de_la_obra` varchar(255) COLLATE utf8_spanish2_ci NOT NULL,
+  `descripsion` varchar(255) COLLATE utf8_spanish2_ci NOT NULL,
+  `sucursal_museo` varchar(255) COLLATE utf8_spanish2_ci NOT NULL,
+  `zona` varchar(55) COLLATE utf8_spanish2_ci NOT NULL,
+  `piso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `obras`
+--
+ALTER TABLE `obras`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `obras`
+--
+ALTER TABLE `obras`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
