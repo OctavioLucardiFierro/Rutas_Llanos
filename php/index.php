abc7ad727c355f75af4f8d6e3714ec6fbc1181e8
OctavioLucardiFierro <octaviolf2016@gmail.com>
2022-09-13 21:03:29 -0300

    Css

--- php/index.php
+++ php/index.php
@@ -9,41 +9,49 @@ $varsesion = $_SESSION['nombre'];
 ?>
 
 <!DOCTYPE html>
-<html>
-<head>
-	<link rel="stylesheet" href="../css/css_general.css">
-		<h1>¿Que va a hacer? <?php   ?></h1>
-		<meta charset="utf-8">
+		<html>
+		<head>
+			<link rel="stylesheet" href="../css/css_general.css">
+			<!--<link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap" rel="stylesheet">-->
+			<link rel="preconnect" href="https://fonts.googleapis.com">
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+			<link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
+				<h1>¿Que va a hacer? <?php $_SESSION['nombre'];  ?></h1>
+				<meta charset="utf-8">
 
-<br><br>
+		<br><br>
 
-</head>
-<body>
+		</head>
+		<body>
+		<div class="container1">
 
-<a href="form.php"><button>INERTAR OBRA</button></a>
-<br>
-<br>
-<a href="form.php"><button>MODIFICAR OBRA</button></a>
-<br>
-<br>
-<a href="form.php"><button>BORRAR OBRA</button></a>
-<br>
-<br>
+				<a href="form.php"><button>INERTAR OBRA</button></a>
+			<br>
+			<br>
+				<a href="form.php"><button>MODIFICAR OBRA</button></a>
+			<br>
+			<br>
+				<a href="form.php"><button>BORRAR OBRA</button></a>
+			<br>
+			<br>
 
-	<?php
-	if ($varsesion == null || $varsesion = '')
-		{
+					<?php
+			if ($varsesion == null || $varsesion = '')
+				{
+					?>
+						<a href="form-login.php"><button>INICIAR SESION</button></a>
+					<?php
+				}
+			else
+				{
+					?>
+						<a href="cerrar.php"><button>CERRAR SESION</button></a>
+					<?php
+				}
 			?>
-				<a href="form-login.php"><button>INICIAR SESION</button></a>
-			<?php
-		}
-		else
-		{
-			?>
-				<a href="cerrar.php"><button>CERRAR SESION</button></a>
-			<?php
-		}
-	?>
+		</div>
+		
 
 
-</body>
+		</body>
+</html>
