9cd9560e8aa17caf8bda1daf81edaf4c70db2e86
ipopotamo <ignaciobarbieri2003@gmail.com>
2022-09-16 21:35:19 -0300

    pipo

--- php/insetCom.php
+++ php/insetCom.php
@@ -1,8 +1,7 @@
 <?php
     include("conexion.php");
 
-if (isset($_POST['boton'])) {
-    if (strlen($_POST['nombre']) >= 1 && strlen($_POST['comentario']) >= 1) {
+
 	    
         $nombre = trim($_POST['nombre']);
 	    $com    = trim($_POST['comentario']);
@@ -10,7 +9,6 @@ if (isset($_POST['boton'])) {
 	    $consulta  = "INSERT INTO `comentarios`(`usuarios`, `comentario`, `fecha_comentario`) VALUES ('$nombre','$com', NOW())";
 	    
 	    $resultado = mysqli_query($conex,$consulta);
-    }
-}
+
 
 ?>
